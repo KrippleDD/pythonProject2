e106d239cc8f733aa78e3a1ddf3820469d479287
KrippleDD <kirill.drik.717@gmail.com>
2022-05-26 22:52:26 +0300

    homeworkdone

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-# ДЗ №3 Парты и школьники
+#ДЗ №3 Парты и школьники
 
 
-kidsinclass1 = int(input(20))
-kidsinclass2 = input(27)
-kidsinclass3 = input(30)
 
-answer1 = kidsinclass1 // 2
-answer2 = kidsinclass2 % 2
-answer3 = kidsinclass3 // 2
+class1 = int(input(20))
+class2 = int(input(27))
+class3 = int(input(30))
+
+
+
+
+answer1 = class1 // 2
+answer2 = class2 % 2
+answer3 = class3 // 2
 
 print('Ответ1', answer1)
 print('Ответ2', answer2)
